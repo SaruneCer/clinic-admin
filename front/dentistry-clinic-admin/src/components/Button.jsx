62ca9e6385b2fdc4c9099b0c0ef998a0e6dee81b
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-11 23:05:46 +0300

    Edited Button component

--- front/dentistry-clinic-admin/src/components/Button.jsx
+++ front/dentistry-clinic-admin/src/components/Button.jsx
@@ -1,15 +1,8 @@
 import "../styles/button.css";
 
 export function Button({ buttonText, onClick }) {
-  const isDeleteOrCancel = buttonText === "DELETE" || buttonText === "Cancel";
-
   return (
-    <button
-      className={isDeleteOrCancel ? "delete-cancel-button" : "basic-button"}
-      style={{}}
-      type="button"
-      onClick={onClick}
-    >
+    <button className="basic-button" style={{}} type="button" onClick={onClick}>
       {buttonText}
     </button>
   );
