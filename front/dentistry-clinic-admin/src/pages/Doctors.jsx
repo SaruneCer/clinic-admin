5a4b7b8afe54cdd2efb100355bbe353c5bf24a26
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-11 23:04:21 +0300

    Updated Doctors page by adding ADD button, imported and used AddFormModal

--- front/dentistry-clinic-admin/src/pages/Doctors.jsx
+++ front/dentistry-clinic-admin/src/pages/Doctors.jsx
@@ -1,16 +1,17 @@
+import { useState, useEffect } from "react";
 import { useGetData } from "../customHooks/useGetData";
 import { ItemBox } from "../components/ItemBox";
-import { useEffect } from "react";
+import { Button } from "../components/Button";
+import { AddFormModal } from "../components/AddFormModal";
 
 export function Doctors() {
   const { data: allDoctors, loading, rerender } = useGetData("doctors");
-  console.log("Data:", allDoctors);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {}, [allDoctors]);
 
   const handleDeleteClick = (doctor) => {
     console.log("Deleting doctor:", doctor);
-
     rerender();
   };
 
@@ -18,25 +19,52 @@ export function Doctors() {
     console.log("Editing doctor with ID:", doctorId);
   };
 
+  const handleAddClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
 
   if (!allDoctors || allDoctors.length === 0) {
-    return <p>No doctors available</p>;
+    return (
+      <main>
+        <header>
+          <Button buttonText={"+ ADD DOCTOR"} onClick={handleAddClick} />
+        </header>
+        <p>No doctors available</p>
+      </main>
+    );
   }
 
   return (
     <main>
-      {allDoctors.map((doctor) => (
-        <ItemBox
-          key={doctor._id}
-          item={doctor}
-          itemType="doctor"
-          handleDeleteClick={handleDeleteClick}
-          handleEditClick={handleEditClick}
+      <header>
+        <Button buttonText={"+ ADD DOCTOR"} onClick={handleAddClick} />
+      </header>
+      <div id="item-box-container">
+        {allDoctors.map((doctor) => (
+          <ItemBox
+            key={doctor._id}
+            item={doctor}
+            itemType="doctor"
+            handleDeleteClick={handleDeleteClick}
+            handleEditClick={handleEditClick}
+          />
+        ))}
+      </div>
+      {isModalOpen && (
+        <AddFormModal
+          resource="doctors"
+          onClose={handleCloseModal}
+          rerender={rerender}
         />
-      ))}
+      )}
     </main>
   );
 }
