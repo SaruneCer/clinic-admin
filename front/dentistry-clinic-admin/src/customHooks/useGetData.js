8d1efaf80292c2f7aff9440ae5f5772688a2b7f8
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-11 22:19:50 +0300

    Created custom hook, for fetching data

---
+++ front/dentistry-clinic-admin/src/customHooks/useGetData.js
@@ -0,0 +1,47 @@
+import { useState, useEffect, useCallback } from "react";
+
+const BASE_URL = "http://localhost:8080/dentistry_clinic_admin/";
+
+export function useGetData(resource) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${BASE_URL}${resource}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      const validData = result.filter((item) => {
+        if (resource === 'doctors' || resource === 'patients') {
+          return item.name && item.contactInfo?.email && item.contactInfo?.phone;
+        } else if (resource === 'procedures') {
+          return item.name && item.duration && item.price;
+        } else if (resource === 'appointments') {
+          return item.doctorName && item.patientName && item.procedureName && item.report;
+        } else if (resource === 'schedules') {
+          return item.doctorName && item.patientId && item.appointmentDate && item.status;
+        }
+        return true; 
+      });
+      setData(validData);
+      setLoading(false);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+      setLoading(false);
+    }
+  }, [resource]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+
+  const rerender = () => {
+    fetchData();
+  };
+
+  return { data, loading, setData, rerender };
+}
