a8aad1918d81a5f339e0264528f9bc203a8331ed
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-11 21:47:15 +0300

    Created Doctors page, in which all the doctors from database is rendered

--- front/dentistry-clinic-admin/src/pages/Doctors.jsx
+++ front/dentistry-clinic-admin/src/pages/Doctors.jsx
@@ -1,3 +1,42 @@
+import { useGetData } from "../customHooks/useGetData";
+import { ItemBox } from "../components/ItemBox";
+import { useEffect } from "react";
+
 export function Doctors() {
-    return(<></>)
+  const { data: allDoctors, loading, rerender } = useGetData("doctors");
+  console.log("Data:", allDoctors);
+
+  useEffect(() => {}, [allDoctors]);
+
+  const handleDeleteClick = (doctor) => {
+    console.log("Deleting doctor:", doctor);
+
+    rerender();
+  };
+
+  const handleEditClick = (doctorId) => {
+    console.log("Editing doctor with ID:", doctorId);
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!allDoctors || allDoctors.length === 0) {
+    return <p>No doctors available</p>;
+  }
+
+  return (
+    <main>
+      {allDoctors.map((doctor) => (
+        <ItemBox
+          key={doctor._id}
+          item={doctor}
+          itemType="doctor"
+          handleDeleteClick={handleDeleteClick}
+          handleEditClick={handleEditClick}
+        />
+      ))}
+    </main>
+  );
 }
