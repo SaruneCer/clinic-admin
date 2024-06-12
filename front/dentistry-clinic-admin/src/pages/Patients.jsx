08aada413b3ce94e1b7d5f884308767049aac516
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-12 23:10:58 +0300

    Created Patients.jsx page, to render all patients

--- front/dentistry-clinic-admin/src/pages/Patients.jsx
+++ front/dentistry-clinic-admin/src/pages/Patients.jsx
@@ -1,3 +1,65 @@
+import { Link } from "react-router-dom";
+import { useGetData } from "../customHooks/useGetData";
+import { Button } from "../components/Button";
+import { AddFormModal } from "../components/AddFormModal";
+import { useState } from "react";
+
 export function Patients() {
-    return(<></>)
+    const { data: patients, loading, rerender } = useGetData("patients");
+    const [isAddFormModalOpen, setIsAddFormModalOpen] = useState(false);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  patients.sort((a, b) => a.lastname.localeCompare(b.lastname));
+
+  let currentLetter = null;
+  const patientElements = [];
+
+  patients.forEach((patient, index) => {
+    const lastName = patient.lastname;
+    const firstLetter = lastName[0].toUpperCase();
+    let header = null;
+
+    if (firstLetter !== currentLetter) {
+      header = <h2 key={`header-${firstLetter}`}>{firstLetter}</h2>;
+      currentLetter = firstLetter;
+      patientElements.push(header);
+    }
+
+    patientElements.push(
+      <Link
+        key={`link-${index}`}
+        to={`/dentistry-clinic-admin/patient-records/${patient._id.$oid}`}
+      >
+        {patient.name} {patient.lastname}
+      </Link>,
+      <br key={`br-${index}`} />
+    );
+  });
+    
+  const handleAddClick = () => {
+    setIsAddFormModalOpen(true);
+  };
+    
+  const handleAddFormCloseModal = () => {
+    setIsAddFormModalOpen(false);
+  };
+
+  return (
+    <main>
+        <header>
+        <Button buttonText={"+ ADD PATIENT"} onClick={handleAddClick} />
+      </header>
+          <div>{patientElements}</div>
+          {isAddFormModalOpen && (
+        <AddFormModal
+          resource="patients"
+          onClose={handleAddFormCloseModal}
+          rerender={rerender}
+        />
+      )}
+    </main>
+  );
 }
