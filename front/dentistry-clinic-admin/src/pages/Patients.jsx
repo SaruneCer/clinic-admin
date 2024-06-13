0faf2b189bd2d276102f13a842f3d05a5ec4c345
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-13 18:35:58 +0300

    Implemented SearchInput in Patients page header element

--- front/dentistry-clinic-admin/src/pages/Patients.jsx
+++ front/dentistry-clinic-admin/src/pages/Patients.jsx
@@ -3,11 +3,14 @@ import { useGetData } from "../customHooks/useGetData";
 import { Button } from "../components/Button";
 import { AddFormModal } from "../components/AddFormModal";
 import { useState } from "react";
+import { useSearch } from "../customHooks/useSearch";
+import { SearchInput } from "../components/SearchInput";
 import "../styles/patients.css";
 
 export function Patients() {
   const { data: patients, loading, rerender } = useGetData("patients");
   const [isAddFormModalOpen, setIsAddFormModalOpen] = useState(false);
+  const [searchValue, setSearchValue, filteredPatients] = useSearch(patients);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -15,7 +18,7 @@ export function Patients() {
 
   patients.sort((a, b) => a.lastname.localeCompare(b.lastname));
 
-  const groupedPatients = patients.reduce((acc, patient) => {
+  const groupedPatients = filteredPatients.reduce((acc, patient) => {
     const firstLetter = patient.lastname[0].toUpperCase();
     if (!acc[firstLetter]) {
       acc[firstLetter] = [];
@@ -24,29 +27,33 @@ export function Patients() {
     return acc;
   }, {});
 
-
   const letters = Object.keys(groupedPatients);
 
-
-  const patientElements = Object.entries(groupedPatients).map(([letter, patients]) => (
-    <div key={`group-${letter}`} id={`section-${letter}`} className="patient-group">
-      <div className="letter-header">
-        <h2>{letter}</h2>
+  const patientElements = Object.entries(groupedPatients).map(
+    ([letter, patients]) => (
+      <div
+        key={`group-${letter}`}
+        id={`section-${letter}`}
+        className="patient-group"
+      >
+        <div className="letter-header">
+          <h2>{letter}</h2>
+        </div>
+        <div className="patient-names">
+          {patients.map((patient) => (
+            <div key={`patient-${patient._id}`} className="patient-item">
+              <Link
+                className="patient-name-link"
+                to={`/dentistry-clinic-admin/patient-records/${patient._id}`}
+              >
+                {patient.name} {patient.lastname}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="patient-names">
-        {patients.map((patient) => (
-          <div key={`patient-${patient._id.$oid}`} className="patient-item">
-            <Link
-              className="patient-name-link"
-              to={`/dentistry-clinic-admin/patient-records/${patient._id.$oid}`}
-            >
-              {patient.name} {patient.lastname}
-            </Link>
-          </div>
-        ))}
-      </div>
-    </div>
-  ));
+    )
+  );
 
   const handleLetterClick = (letter) => {
     const targetSection = document.getElementById(`section-${letter}`);
@@ -67,10 +74,18 @@ export function Patients() {
     <main>
       <header>
         <Button buttonText={"+ ADD PATIENT"} onClick={handleAddClick} />
+        <SearchInput
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
       </header>
       <div className="alphabet-nav">
         {letters.map((letter) => (
-          <button key={`nav-${letter}`} onClick={() => handleLetterClick(letter)} className="nav-button">
+          <button
+            key={`nav-${letter}`}
+            onClick={() => handleLetterClick(letter)}
+            className="nav-button"
+          >
             {letter}
           </button>
         ))}
