d324c7be0a5b53819bced08f9dd5287e88c7bad6
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-15 14:55:53 +0300

    Updated EditInfoModal component

--- front/dentistry-clinic-admin/src/components/EditInfoModal.jsx
+++ front/dentistry-clinic-admin/src/components/EditInfoModal.jsx
@@ -1,4 +1,4 @@
-import { Button } from './Button'; 
+import { Button } from "./Button";
 import { useState } from "react";
 import "../styles/edit-info-modal.css";
 
@@ -38,35 +38,46 @@ export function EditInfoModal({ dataInfo, onSave, onClose }) {
   };
 
   const renderFormFields = (data, parentKey = null) => {
-    return Object.keys(data).map((key) => {
-      const value = data[key];
-      if (key === "_id") {
-        return null;
-      }
-      if (typeof value === "object" && !Array.isArray(value) && value !== null) {
-        return renderFormFields(value, key);
-      }
-      return (
-        <div className="edit-form-input" key={key}>
-          <label htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
-          <input
-            type={typeof value === "number" ? "number" : "text"}
-            id={key}
-            name={key}
-            value={value}
-            onChange={(e) => handleChange(e, parentKey)}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </div>
-      );
-    }).filter(Boolean);
+    return Object.keys(data)
+      .map((key) => {
+        const value = data[key];
+        if (key === "_id" || key === "medicalHistory") {
+          return null;
+        }
+        if (
+          typeof value === "object" &&
+          !Array.isArray(value) &&
+          value !== null
+        ) {
+          return renderFormFields(value, key);
+        }
+
+        return (
+          <div className="edit-form-input" key={key}>
+            <label htmlFor={key}>
+              {key.charAt(0).toUpperCase() + key.slice(1)}:
+            </label>
+            <input
+              type={typeof value === "number" ? "number" : "text"}
+              id={key}
+              name={key}
+              value={value}
+              onChange={(e) => handleChange(e, parentKey)}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </div>
+        );
+      })
+      .filter(Boolean);
   };
 
   return (
     <div className="modal">
       <div className={`modal-content ${isEditing ? "is-editing" : ""}`}>
-        <span className="close-button" onClick={onClose}>&times;</span>
+        <span className="close-button" onClick={onClose}>
+          &times;
+        </span>
         <h2>Edit Information</h2>
         <form className="edit-form">
           {renderFormFields(editedInfo)}
