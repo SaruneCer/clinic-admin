f5052903755991752db592cb3034a1864956d778
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-25 11:31:11 +0300

    Updated AppointmentBox

--- front/dentistry-clinic-admin/src/components/AppointmentBox.jsx
+++ front/dentistry-clinic-admin/src/components/AppointmentBox.jsx
@@ -1,10 +1,14 @@
 export function AppointmentBox({ item }) {
   return (
     <div className="appointment-box">
-      <div className="appointment-date">{item.appointmentDate}</div>
-      <div className="appointment-doctor">{item.doctorName}</div>
+      <div className="appointment-date">
+        <strong>Date</strong> | {item.appointmentDate}
+      </div>
+      <div className="appointment-doctor">
+        <strong>Doctor</strong> | {item.doctorName}
+      </div>
+      <br />
       <div className="appointment-report">{item.report}</div>
     </div>
   );
 }
-
