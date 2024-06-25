c151fb6a47bd8d5dc2b6ce46d4a461d5ccdc61ed
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-25 11:32:29 +0300

    Updated SearchInput

--- front/dentistry-clinic-admin/src/components/SearchInput.jsx
+++ front/dentistry-clinic-admin/src/components/SearchInput.jsx
@@ -1,19 +1,17 @@
 export function SearchInput({ searchValue, setSearchValue }) {
-    const handleChange = (e) => {
-      setSearchValue(e.target.value);
-    };
-  
-    return (
-      <div className="search-input-container">
-       
-          <input
-            type="text"
-            id="search-input"
-                    value={searchValue}
-                    placeholder="Search"
-            onChange={handleChange}
-          ></input>
-        
-      </div>
-    );
-  }
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <div className="search-input-container">
+      <input
+        type="text"
+        value={searchValue}
+        placeholder="Search"
+        onChange={handleChange}
+        className="search-input"
+      ></input>
+    </div>
+  );
+}
