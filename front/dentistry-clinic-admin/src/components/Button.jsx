84339716778e19026b6cc2eaf7183f8a2228707b
s.cernikeviciene <sarune.ku@gmail.com>
2024-06-25 11:31:20 +0300

    Updated Button

--- front/dentistry-clinic-admin/src/components/Button.jsx
+++ front/dentistry-clinic-admin/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import "../styles/button.css";
 
 export function Button({ buttonText, onClick }) {
+  let className = "basic-button";
+
+  if (buttonText === "DELETE APPOINTMENT") {
+    className = "delete-appointment-button";
+  }
+
   return (
-    <button className="basic-button" style={{}} type="button" onClick={onClick}>
+    <button className={className} style={{}} type="button" onClick={onClick}>
       {buttonText}
     </button>
   );
